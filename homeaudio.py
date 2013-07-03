2bcf38655e0797fc630e07213741682b0c238f4f
themacks <mark@markmccarthy.net>
2013-07-03 15:31:23 -0400

    Removed name parameter. Added URL handlers for pandora and airplay.

--- homeaudio.py
+++ homeaudio.py
@@ -5,12 +5,22 @@ import webpy.web as web
 render = web.template.render('templates/')
 
 urls = (
-    '/(.*)', 'index'
+    '/', 'index',
+    '/pandora', 'pandora',
+    '/airplay', 'airplay'
 )
 
 class index:
-    def GET(self, name):
-        return render.index(name)
+    def GET(self):
+        return render.index()
+        
+class pandora:
+    def GET(self):
+        return "Pandora!"
+        
+class airplay:
+    def GET(self):
+        return "Airplay!"
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ echo "Getting webpy..."
 git clone https://github.com/webpy/webpy.git
 touch ./webpy/__init__.py
 
+echo "Installing flup..."
+sudo apt-get install python-setuptools
+sudo easy_install flup
